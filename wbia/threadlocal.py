33d7b8d998d42c9736f21ba602bfba9a9ca753ae
Michael Mulich <michael.mulich@gmail.com>
2020-08-09 18:00:01 -0700

    Add a thread-local manager
    This provides us with a framework agnostic bit of logic to place our
    globals as thread-locals. Essentially, any application needs to have
    one or more global bits, this providess a central location to place
    those bits in a stacked format.
    
    This is a paraphrased form of the threadlocal module found in the
    Pyramid framework. I've gone this route, because the thread local
    solution provided by werkzeug is specifically tailored for web server
    use. The Pyramid solution is generalized enough--and I've generalized
    it even more--to work in just about any situation.

---
+++ wbia/threadlocal.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Thread-Local - management of data between threads and parallel processes
+"""
+import threading
+
+
+__all__ = (
+    'get_current_controller',
+    'get_current_settings',
+    'manager',
+)
+
+
+class ThreadLocalManager(threading.local):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, info):
+        self.stack.append(info)
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+
+    def get(self):
+        return self.stack[-1]
+
+    def clear(self):
+        self.stack[:] = []
+
+
+manager = ThreadLocalManager()
+
+
+def get_current_controller():
+    """Return the active controller"""
+    return manager.get()['controller']
+
+
+def get_current_settings():
+    """Return the active settings"""
+    return manager.get()['settings']
