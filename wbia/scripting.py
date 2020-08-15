fa18b2c37932367fb6dae39de85e5acd3a8467ad
Michael Mulich <michael.mulich@gmail.com>
2020-08-14 20:40:16 -0700

    Add a scripting preparation function
    This loads the settings and application controller(s). The intended usage
    is to be used as a contextual factory within a script, including the
    main runtime script. The following is an example of what might show up
    inside the body of a script's main function:
    
    ```python
    with prepare(optional_settings_from_argv) as app_context:
        print(app_context['db']metadata.database.version)
    ```
    
    Note, the application context's is bound to change. For now we're
    database focused. Later we may have a better application startup
    controller in place of some of the other minor controllers.

---
+++ wbia/scripting.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""\
+Bootstrapping functionality for commandline invocation
+"""
+from .threadlocal import manager
+from .control.IBEISControl import IBEISController as Controller
+
+
+__all__ = ('prepare',)
+
+
+def prepare(settings=None, controller=None):
+    """
+    Initializes the Controller (heart of the application) for use
+    in a scripted setting. This returns a dictionary of locals
+    including a controller object, settings data and closer function.
+
+    The closer function should be used at the end of your script
+    for data safety. Tip, use this function as a context-manager
+    to automatically call the closer.
+
+    This function places the settings and controller on the thread-local stack.
+    You can access the data from anywhere in your program,
+    because the variables are essentially global.
+
+    Args:
+        controller (Controller): an instantiated controller object
+
+    Returns:
+        dict: locals to be used within a scripting setting
+    """
+    # Discover settings and configuration
+    if not settings:
+        settings = discover_settings()
+
+    # Build the controller
+    if not controller:
+        controller = Controller.from_uri(settings['db.runtime.uri'])
+
+    def closer():  # pragma: no cover
+        controller.close()
+
+    # Push the controller onto the threadlocal stack
+    locals_ = {'controller': controller, 'settings': settings}
+    manager.push(locals_)
+
+    # Prepare the locals for return
+    return AppContext(closer=closer, **locals_)
+
+
+class AppContext(dict):
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        getattr(self, 'closer', lambda: None)()
